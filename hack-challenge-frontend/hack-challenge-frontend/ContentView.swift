611fc4a51a5c22037b96f57ebc725c280d392194
Aaron <az394@cornell.edu>
2024-12-03 21:40:06 -0500

    boilerplate

---
+++ hack-challenge-frontend/hack-challenge-frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hack-challenge-frontend
+//
+//  Created by Aaron Zhu on 12/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hack-challenge-frontend/hack-challenge-frontend/hack_challenge_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  hack_challenge_frontendApp.swift
+//  hack-challenge-frontend
+//
+//  Created by Aaron Zhu on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct hack_challenge_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
